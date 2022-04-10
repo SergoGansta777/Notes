3d4ef62f02562f5d6f90d45761f25c7e47ce2c28
SergoGansta777 <sergo777ser777@gmail.com>
2022-04-10 12:59:08 +0300

    vault backup: 2022-04-10 12:59:08

---
+++ VScodeProgramming/NewTryCsharp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Programm
+{
+    class Programm
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Hello from linux!");
+        }
+    }
+}
