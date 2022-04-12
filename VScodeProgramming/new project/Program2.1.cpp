c6ce0e3e2a3a2aac285870dec7b7bcb78f09544c
SergoGansta777 <sergo777ser777@gmail.com>
2022-04-12 22:58:58 +0300

    vault backup: 2022-04-12 22:58:58

--- VScodeProgramming/new project/Program2.1.cpp
+++ VScodeProgramming/new project/Program2.1.cpp
@@ -1,45 +1,77 @@
 #include <iostream>
-#include <math.h>
+#include <ctime>
+#include <random>
+#include <algorithm>
 
 using namespace std;
 
-int Main()
+void bsort(int* start, int* end)
 {
-    int N,M, Sum=0;
-    cin >> N;
-    int array[N];
-    cin >> M;
-    for(int i = 1; i<=N;i++)
-    {
-        cin >> array[i];
-    }
-    int maxvariants = pow(N,2);
-    for(int i = 1; i<maxvariants;i++)
-    {
-        int temp = i, Sum=0;
-        for(int j = 1;j<=N;j++)
-        {
-            if(temp & 1)
-            {
-                Sum+=array[j];
-            }
-            temp>>1;
-        }
-        if(Sum==M)
-        {
-            cout << "{ ";
-           for(int j = 1;j<=N;j++)
-            {
-                if(temp & 1)
-                {
-                    cout << array[j] << ", ";
-                }
-                temp>>1;
-            }
-            cout << "}" ;
-            return 0;
-        }
-    }
-    cout << "None" << endl;
-    return 0;
+	for (int i = 0; i < (end - start) - 1; ++i)
+		for (int* j = start; j < end - 1; j++)
+		{
+			if (*j > *(j + 1))
+			{
+				int buf = *j;
+				*j = *(j + 1);
+				*(j + 1) = buf;
+			}
+		}
+}
+
+void csort(int* start, int* end, int d)
+{
+	int* temp = new int[d + 1]{};
+	for (int* i = start; i < end; i++)
+	{
+		temp[*i]++;
+	}
+	int elem = 0;
+	for (int *i = temp; i < temp + d + 1; i++)
+	{
+		for (int j = 0; j < *i; j++)
+		{
+			start[elem] = i - temp;
+			++elem;
+		}
+	}
+	delete[] temp;
+}
+
+int main(void)
+{
+	cout << "n: " << endl;
+	int n; cin >> n;
+	cout << "d: " << endl;
+	int d; cin >> d;
+	int* arr = new int[n];
+	
+	for (long long i = 0; i < n; i++)
+	{
+		arr[i] = rand() % d;
+
+		//output
+		/*cout << arr[i] << " ";*/
+	}
+	cout << endl;
+
+	clock_t start = clock();
+
+	//type function here
+	sort(arr,arr+n);
+
+	
+	clock_t end = clock();
+
+	//output
+	/*for (long long i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;*/
+
+	cout << "time: " << (double)(end - start) / CLOCKS_PER_SEC << endl;
+
+	delete[] arr;
+	return 0;
 }
