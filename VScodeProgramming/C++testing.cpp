c3436808cc8c9069dc2abf9b8c7e26a20a3afea8
SergoGansta777 <sergo777ser777@gmail.com>
2022-04-28 21:31:48 +0300

    vault backup: 2022-04-28 21:31:48

--- VScodeProgramming/C++testing.cpp
+++ VScodeProgramming/C++testing.cpp
@@ -4,31 +4,58 @@
 
 using namespace std;
 
-int main()
+static void Example1()
 {
     int a,b;
-    
-    string str;
-    char* test = new char[256];
-    cin.getline(test,256);
-
     ifstream in("testingC++FilereadingFutures.txt");
     if(!in)
     {
         cout << "Файл не удается открыть\n";
-        return 1;
+        return ;
     }
     in >> a >> b;
-    ofstream out1("testingC++FilereadingFutures.txt");
+    ofstream out1("result.txt");
     if(!out1)
     {
         cout << "Файл не удается открыть\n";
-        return 1;
+        return ;
     }
     out1 << a+b;
-    cout << a+b;
-    // getline(in,str);
-    // cout << str << endl;
+
     in.close();
+}
+static void Example2()
+{
+    ifstream in("text.txt");
+    if(!in)
+    {
+        cout << "Файл не удается открыть\n";
+        return ;
+    }
+
+    char* str = new char[1000];
+    //in.ignore();
+    in.getline(str,1000,'\n');
+    cout << str;
+    in.close();
+    ofstream out1("resultText.txt");
+    if(!out1)
+    {
+        cout << "Файл не удается открыть\n";
+        return ;
+    }
+    for(int i = 0;str[i]!='\0';i++)
+    {
+         if(str[i]!=' ') {
+             out1 << str[i];
+             cout << str[i];
+         }
+    } 
+    out1.close();
+    return;
+}
+int main()
+{
+    Example2();
         return 0;
 }
