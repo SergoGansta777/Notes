d051925006e4e620bc44e8b081603cf24c3206e9
SergoGansta777 <sergo777ser777@gmail.com>
2022-05-03 11:23:17 +0300

    vault backup: 2022-05-03 11:23:17

--- VScodeProgramming/C++testing.cpp
+++ VScodeProgramming/C++testing.cpp
@@ -5,56 +5,6 @@
 
 using namespace std;
 
-static void Example1()
-{
-    int a,b;
-    ifstream in("testingC++FilereadingFutures.txt");
-    if(!in)
-    {
-        cout << "Файл не удается открыть\n";
-        return ;
-    }
-    in >> a >> b;
-    ofstream out1("result.txt");
-    if(!out1)
-    {
-        cout << "Файл не удается открыть\n";
-        return ;
-    }
-    out1 << a+b;
-
-    in.close();
-}
-static void Example2()
-{
-    ifstream in("text.txt");
-    if(!in)
-    {
-        cout << "Файл не удается открыть\n";
-        return ;
-    }
-
-    char* str = new char[1000];
-    //in.ignore();
-    in.getline(str,1000,'\0');
-    cout << str;
-    in.close();
-    ofstream out1("resultText.txt");
-    if(!out1)
-    {
-        cout << "Файл не удается открыть\n";
-        return ;
-    }
-    for(int i = 0;str[i]!='\0';i++)
-    {
-         if(str[i]!=' ') {
-             out1 << str[i];
-             cout << str[i];
-         }
-    } 
-    out1.close();
-    return;
-}
 static void Number20()
 {
     ifstream in("testingC++FilereadingFutures.txt");
@@ -191,10 +141,9 @@ static void Number48()
     ifstream inf("listOfMembers.txt");
     if(!inf)
     {
-        cout << "Файл не удается открыть\n";
+        cout << "Входной файл не удается открыть\n";
         return ;
     }
-    char list [n][100];
     char** list1 = new char*[50];
     for(int i=0;i<50;i++)
     {
@@ -203,15 +152,17 @@ static void Number48()
     }
     inf.close();
     Sort(list1,n);
+
     int* valueOfmembers= new int[n];
     GetNumericValueOfMembers(list1,valueOfmembers,n);
     int Place1stValue = GetPriseValue(valueOfmembers,n);
     int Place2stValue = GetPriseValue(valueOfmembers,n,Place1stValue);
     int Place3stValue = GetPriseValue(valueOfmembers,n,Place1stValue,Place2stValue);
+
     ofstream out1("resultText.txt");
     if(!out1)
     {
-        cout << "Файл не удается открыть\n";
+        cout << "Выходной файл не удается открыть\n";
         return ;
     }
     if(CheckLess25(valueOfmembers,n,Place1stValue,Place2stValue,Place3stValue))
@@ -233,11 +184,17 @@ static void Number48()
             if(valueOfmembers[i]==Place3stValue) out1 << GetName(list1,n,i) << endl;
         }
     }else{
+        bool isExist = false;
          out1 << "Winners:" << endl;
          for(int i = 0;i<n;i++)
          {
-             if(valueOfmembers[i]>=400) out1 << GetName(list1,n,i) << endl;
+             if(valueOfmembers[i]>=400) 
+             {
+                 out1 << GetName(list1,n,i) << endl;
+                 isExist = true;
+             }
          }
+         if(!isExist) out1 << "none" << endl;
     }
     
     out1.close();
